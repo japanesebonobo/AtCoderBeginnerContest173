2d5c1f1b45eacc0eff503c600fb3f6139bea8198
japanesebonobo <tomi119119@gmail.com>
2020-07-05 21:49:59 +0900

    Update A

--- src/com/company/A.java
+++ src/com/company/A.java
@@ -1,5 +1,26 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class A {
-    
+    public static void main(String[] var0) {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+        int ans = 0;
+
+        if (N < 1000) {
+            ans = 1000 - N;
+        } else if (N % 1000 == 0){
+            System.out.println(0);
+            System.exit(0);
+        } else {
+            String t = String.valueOf(N);
+            char c = t.charAt(0);
+            int thousand = Character.getNumericValue(c)*1000;
+            ans = 1000 - (N - thousand);
+        }
+
+        System.out.println(ans);
+    }
 }
